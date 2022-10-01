f3248ce2c9dd0571437abf0f2a086bfc12f96838
tatsuki-ruska <tatsukinakagawa.ruska@gmail.com>
2022-10-01 23:15:28 +0900

    css fix

--- components/Header.tsx
+++ components/Header.tsx
@@ -37,7 +37,7 @@ export function Header({ app }: { app: AppMeta }): JSX.Element {
               <div className="logo-wrap">
                   <p className="logo">
                     <Link href="/">
-                      <a><img src="../img/img-logo-01.png" /></a>
+                      <a><img src="/img/img-logo-01.png" /></a>
                     </Link>
                   </p>
                   <div className="openbtn"><span></span><span></span><span></span></div>
