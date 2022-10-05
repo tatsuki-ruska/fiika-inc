96a6069f4147f17aa71128bff27c480fa4a2834e
tatsuki-ruska <tatsukinakagawa.ruska@gmail.com>
2022-10-06 00:56:20 +0900

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,6 @@
 import '../styles/globals.css'
 import '../styles/style.css'
 import '../js/jquery-3.4.1.min'
-import '../js/main'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
