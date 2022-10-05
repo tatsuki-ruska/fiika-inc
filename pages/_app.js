82fbc0d3996a571076a193f385102bd017b9dd24
tatsuki-ruska <tatsukinakagawa.ruska@gmail.com>
2022-10-06 02:01:07 +0900

    css add

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,6 @@
 import '../styles/globals.css'
 import '../styles/style.css'
-
-import React from 'react';
-import { findDOMNode } from 'react-dom';
-import $ from 'jquery';
+import '../styles/notosans.css'
 
 function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />
