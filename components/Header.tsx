a36b4d00c7857099edc796bef0f96953674dacf7
tatsuki-ruska <tatsukinakagawa.ruska@gmail.com>
2022-10-10 23:41:51 +0900

    Update Header.tsx

--- components/Header.tsx
+++ components/Header.tsx
@@ -46,7 +46,7 @@ export function Header({ app }: { app: AppMeta }): JSX.Element {
     if (window.location.search.indexOf('?blog') !== -1) {
       clearTimeout();
     }
-  }, 100);
+  }, 500);
 
   // let i = 0;
 
